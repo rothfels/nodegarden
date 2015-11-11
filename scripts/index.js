28b31c51e541c9d457072f7d4df88c2550898c84
John Rothfels <john.rothfels@gmail.com>
2015-11-11 14:39:16 -0800

    remove moon and night mode auto-toggle

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,11 @@ import NodeGarden from './nodegarden'
 
 var pixelRatio = window.devicePixelRatio
 var $container = document.getElementById('container')
-var $moon = document.getElementsByClassName('moon')[0]
 
 var nodeGarden = new NodeGarden($container)
 
 // start simulation
 nodeGarden.start()
-
-// trigger nightMode automatically
-var date = new Date()
-if (date.getHours() > 18 || date.getHours() < 6) {
-  nodeGarden.toggleNightMode()
-}
 
 var resetNode = 0
 
@@ -26,5 +19,4 @@ $container.addEventListener('click', function (e) {
   nodeGarden.nodes[resetNode].reset({x: e.pageX * pixelRatio, y: e.pageY * pixelRatio, vx: 0, vy: 0})
 })
 
-$moon.addEventListener('click', () => { nodeGarden.toggleNightMode() })
 window.addEventListener('resize', () => { nodeGarden.resize() })
